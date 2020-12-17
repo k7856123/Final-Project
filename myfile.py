475d5d8f4a1eadd206ee13bd6e8c214e2ade70c3
k7856123 <76186080+k7856123@users.noreply.github.com>
2020-12-17 15:13:10 -0800

    Euler problem 1

--- myfile.py
+++ myfile.py
@@ -1,1 +1,8 @@
+#kennedy becher
+#problem 1
 
+all_sum = 0 #setting sum to zero
+for i in range(1000): #setting range and for statement
+    if (i%3 ==0 or i%5 ==0): # finding multiples of 3 & 5
+        all_sum = all_sum + i 
+print(all_sum)
